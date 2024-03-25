b759703dffd5ec42e089d0a75cfd6a310aac51bb
123456dfg <1074785246@qq.com>
2024-03-25 15:52:37 +0800

    src

---
+++ launch.sh
@@ -0,0 +1,4 @@
+. /opt/ros/humble/setup.sh && colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release && \
+source install/setup.bash && \
+ros2 launch rm_vision_bring_up vision_bringup.launch.py && \
+ros2 launch rosbridge_server rosbridge_websocket_launch.xml
